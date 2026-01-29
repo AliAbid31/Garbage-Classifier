4fe9e228260e0b44e731070c87107997550928aa
Ali Abid <oa_abid@esi.dz>
2026-01-29 22:59:15 +0100

    Update page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -65,7 +65,7 @@ export default function Home() {
     formData.append("file", file);
 
     try {
-      const API_URL = process.env.API_URL || "http://127.0.0.1:8000";
+      const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://127.0.0.1:8000";
       const response = await fetch(`${API_URL}/predict`, {
         method: "POST",
         body: formData,
